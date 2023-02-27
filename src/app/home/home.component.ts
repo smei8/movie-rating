00cee3ab200bb31d8c7c83c709160cb9c2be7633
smei8 <94879898+smei8@users.noreply.github.com>
2023-02-27 17:52:13 -0500

    home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  trendingMovies: any
+
+  constructor(
+    private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getTrendingMoives()
+  }
+
+  getTrendingMoives() {
+    this.http.get('http://localhost:4200/assets/data/trending-movies.json')
+      .subscribe((movies) => {
+        this.trendingMovies = movies
+        console.log(this.trendingMovies)
+      })
   }
 
 }
